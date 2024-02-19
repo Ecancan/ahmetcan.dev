1bf8ab7c3d05b0a58e136c678c1216f1291d99fd
Ahmet CAN <email.ahmetcan@gmail.com>
2024-02-19 21:13:50 +0300

    Change docs texts.

--- src/components/base/common/glowing-image/GlowingImage.tsx
+++ src/components/base/common/glowing-image/GlowingImage.tsx
@@ -4,7 +4,6 @@ import BaseView from '@/components/base/common/base-view/BaseView';
 import { GlowingImageProps } from '@/components/base/common/glowing-image/GlowingImage.interface';
 import { twMerge } from 'tailwind-merge';
 
-// eslint-disable-next-line no-undef
 const GlowingImage: FC<GlowingImageProps> = (props) => {
   const { src, className, blurClassName, ...rest } = props;
 
